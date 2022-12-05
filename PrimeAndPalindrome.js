326af24a47a307daf59df8616b12c8dc1c7902cf
GauravG-K <gauravkanaujiya587@gmail.com>
2022-12-05 23:41:53 +0530

    Prime and also palindrome number

--- PrimeAndPalindrome.js
+++ PrimeAndPalindrome.js
@@ -4,7 +4,7 @@ for(let i=2;i<=N;i++)
 
     let isPrime=true;
     if(i==2)
-    console.log(i,"is PrimeNo")
+    console.log(i+" is Prime and Palindrome Number")
     else {
     let j=2;
     while(j*j<=i)
@@ -17,7 +17,21 @@ for(let i=2;i<=N;i++)
         j++;
     }
     if(isPrime==true)
-    console.log(i,"is PrimeNo")
+	{
+	let rem, temp, final = 0;
+		let number =i;
+		temp = number;
+		while(number>0)
+		{
+			rem=number%10;
+			number=parseInt(number/10);
+			final=final*10+rem;
+		}
+		if(final==temp)
+		{
+		    console.log(i+" is Prime and Palindrome Number")
+		}
+	}
 
     }
 }
